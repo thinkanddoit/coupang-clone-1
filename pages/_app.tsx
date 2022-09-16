384c8df0cd8a8343e9d208488f3e0ab6b6a8db21
easyone <thinkanddoit@naver.com>
2022-09-16 15:44:43 +0900

    feat: react-query defaultOptions 세팅
    - 추가 path 수정 반영
    - 무분별한 refetch 막기위해 false 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
-import "../styles/globals.css";
+import "@styles/globals.css";
 import type { AppProps } from "next/app";
 import { useState } from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnMount: false,
+            refetchOnReconnect: false,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
