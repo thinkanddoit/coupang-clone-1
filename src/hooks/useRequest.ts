16e906a660f0a79eae1ae72c4ca0c957abe4817a
easyone <thinkanddoit@naver.com>
2022-09-17 14:53:51 +0900

    feat: react-query 관련 api 함수 로직 분리
    - useRequest HOOK

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,1 +1,31 @@
-export const useRequest = () => {};
+import {
+  useQuery as useQueryOrigin,
+  useMutation as useMutationOrigin,
+  QueryKey,
+  QueryFunction,
+  UseQueryResult,
+  UseMutationOptions,
+  UseQueryOptions,
+  MutationFunction,
+} from "react-query";
+
+interface useQueryProps {
+  queryKey: QueryKey;
+  queryFn: QueryFunction;
+  options?: UseQueryOptions;
+}
+export function useQuery({
+  queryKey,
+  queryFn,
+  options,
+}: useQueryProps): UseQueryResult {
+  return useQueryOrigin(queryKey, queryFn, { ...options });
+}
+
+interface useMutationProps {
+  mutationFn: MutationFunction;
+  options?: UseMutationOptions;
+}
+export function useMutation({ mutationFn, options }: useMutationProps) {
+  return useMutationOrigin(mutationFn, { ...options });
+}
