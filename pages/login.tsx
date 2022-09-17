31059c62148347c475abd67d195f0ce205691aef
easyone <thinkanddoit@naver.com>
2022-09-17 15:43:57 +0900

    feat: 회원가입, 로그인 페이지 임시 생성
    - Token사용을 위해 임시 auth관련 페이지 생성
    #6

---
+++ pages/login.tsx
@@ -0,0 +1,49 @@
+import { AuthService } from "@services";
+import { Router, useRouter } from "next/router";
+import { useState } from "react";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("test@naver.com");
+  const [password, setPassword] = useState("1234");
+  const router = useRouter();
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridGap: "10px",
+        width: "300px",
+        padding: "20px",
+      }}
+    >
+      <label>
+        <b>아이디(이메일)</b>
+      </label>
+      <input
+        type="text"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label>
+        <b>비밀번호</b>
+      </label>
+      <input
+        type="password"
+        name="pw"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        name="로그인"
+        onClick={() => {
+          AuthService.login(email, password);
+          router.push("/cart");
+        }}
+      >
+        로그인
+      </button>
+    </div>
+  );
+};
+
+export default LoginPage;
