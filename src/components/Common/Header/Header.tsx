6b5fd6b16ad6ff2cdd08f61a0029f7d0961a7286
easyone <thinkanddoit@naver.com>
2022-09-20 21:20:49 +0900

    feat: Header 컴포넌트 구현

---
+++ src/components/Common/Header/Header.tsx
@@ -0,0 +1,11 @@
+import * as S from "./Header.style";
+
+const Header = () => {
+  return (
+    <S.Container>
+      <S.Logo href="https://www.coupang.com/" />
+    </S.Container>
+  );
+};
+
+export default Header;
