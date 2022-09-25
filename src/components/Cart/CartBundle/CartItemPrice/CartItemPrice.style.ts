8df48a0e5b59127faffb35ce21fd11fa065cc4b1
easyone <thinkanddoit@naver.com>
2022-09-26 06:23:13 +0900

    CartItemPrice 컴포넌트 구현

---
+++ src/components/Cart/CartBundle/CartItemPrice/CartItemPrice.style.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-left: 1px solid #e0e0e0;
+`;
+
+export const PriceContainer = styled.div`
+  font-size: 12px;
+  color: #888;
+`;
