9cdc855a6e73a40fdea8828b1e385978f7f1542e
easyone <thinkanddoit@naver.com>
2022-09-28 18:35:39 +0900

    perf: image domains 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "img1a.coupangcdn.com",
+      "thumbnail6.coupangcdn.com",
+      "thumbnail7.coupangcdn.com",
+      "thumbnail8.coupangcdn.com",
+      "thumbnail9.coupangcdn.com",
+      "thumbnail10.coupangcdn.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
