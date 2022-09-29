72bf75019d53304e867e93d2ccd2942e56a21423
easyone <thinkanddoit@naver.com>
2022-09-29 23:26:35 +0900

    feat: checkout Dynamic Routes page 생성
    - id를 query로 받아 동적으로 반영한다.
    - getServerSideProps함수를 이용해 SSR에서 api호출을 통해 data를 미리 받는다.

--- pages/checkout/[id].tsx
+++ pages/checkout/[id].tsx
@@ -1,3 +1,20 @@
-export default function CheckoutPage() {
+import { OrderSheetDataType } from "@customTypes/checkout";
+import { CheckoutService } from "@services";
+
+interface PropsType {
+  data: OrderSheetDataType;
+}
+
+export default function CheckoutPage({ data }: PropsType) {
+  console.log(data);
   return <></>;
+}
+
+export async function getServerSideProps(context: any) {
+  const { id } = context.query;
+  const cookie = context.req?.headers.cookie;
+
+  return {
+    props: { data: await CheckoutService.getOrderSheetInSSR(id, cookie) },
+  };
 }
