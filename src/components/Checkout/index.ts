f93b7060a15834965ea7fcd2e3be24aa610a510a
easyone <thinkanddoit@naver.com>
2022-10-03 00:16:54 +0900

    feat: Checkout page 적용

--- src/components/Checkout/index.ts
+++ src/components/Checkout/index.ts
@@ -1,2 +1,5 @@
 export { default as CheckoutPageLayout } from "./CheckoutPageLayout/CheckoutPageLayout";
 export { default as CheckoutTitle } from "./CheckoutTitle/CheckoutTitle";
+export { default as BuyerInfo } from "./BuyerInfo/BuyerInfo";
+export { default as ReceiverInfo } from "./ReceiverInfo/ReceiverInfo";
+export { default as CheckoutInfo } from "./CheckoutInfo/CheckoutInfo";
