7461b2b40f01b016c87c6b61fb00f878c742cbc3
easyone <thinkanddoit@naver.com>
2022-10-03 00:12:29 +0900

    feat: TotalCheckoutPrice 컴포넌트 생성

---
+++ src/components/Checkout/CheckoutInfo/TotalCheckoutPrice/TotalCheckoutPrice.style.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  display: flex;
+`;
+
+export const TotoalPrice = styled.div`
+  color: #333;
+  font-weight: bold;
+  width: 115px;
+`;
+
+export const CaseToBeEarnedBox = styled.div`
+  width: 115px;
+`;
