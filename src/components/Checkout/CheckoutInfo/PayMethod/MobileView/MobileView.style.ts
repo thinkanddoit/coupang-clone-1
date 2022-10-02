d83a132f94a0ee5ad857143cc9f84ccc6938c89e
easyone <thinkanddoit@naver.com>
2022-10-03 00:11:16 +0900

    feat: MobileView 컴포넌트 생성

---
+++ src/components/Checkout/CheckoutInfo/PayMethod/MobileView/MobileView.style.ts
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  padding: 10px 0;
+`;
+
+export const Span = styled.span`
+  display: inline-block;
+  width: 90px;
+  font-size: 12px;
+  font-weight: 700;
+  margin: 0;
+  vertical-align: middle;
+`;
+
+export const NoOptionWarning = styled.p`
+  display: block;
+  color: #f80600;
+  padding-left: 20px;
+  margin-top: 10px;
+  background-image: url(//img1a.coupangcdn.com/image/www/order2015/icon-alert.png);
+  background-repeat: no-repeat;
+`;
